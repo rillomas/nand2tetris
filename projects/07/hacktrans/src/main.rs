fa25c853e0fccddbd087ec5f44b74d76ef904361
Masato Nakasaka <rillomas@gmail.com>
2021-04-25 11:08:09 +0900

    added initial code for vm translator

---
+++ projects/07/hacktrans/src/main.rs
@@ -0,0 +1,23 @@
+use clap::{AppSettings, Clap};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Seek, Write};
+use std::path::{Path, PathBuf};
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Masato Nakasaka <rillomas@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(short)]
+    input_file: String,
+}
+fn main() -> std::io::Result<()> {
+    let opts = Opts::parse();
+    let input_file_path = Path::new(&opts.input_file);
+    let mut output_file_path = PathBuf::from(input_file_path);
+    output_file_path.set_extension("asm");
+    println!("input: {}", input_file_path.display());
+    println!("output: {}", output_file_path.display());
+    let file = File::open(input_file_path)?;
+    let mut reader = BufReader::new(file);
+    Ok(())
+}
