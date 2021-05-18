64b73cc47b8118cf23ca0e88c6c747f713c3dfdb
Masato Hori <rillomas@gmail.com>
2021-05-18 19:12:08 +0900

    fixed module structure

--- projects/jack_compiler/src/main.rs
+++ projects/jack_compiler/src/main.rs
@@ -3,7 +3,6 @@ use quick_xml::se::to_string;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-mod token;
 
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Masato Nakasaka <rillomas@gmail.com>")]
@@ -20,11 +19,15 @@ fn main() -> std::io::Result<()> {
     // apply tokenization and parsing for all jack files
     for mut io in io_list {
         println!("input: {}", &io.input_file.display());
-        println!("output: {}", &io.output_file.display());
-        let tokens = token::generate_token_list(&mut io.input);
+        let mut output_file_path = io.input_file.clone();
+        let origin_name = jack_compiler::get_origin_name(&io.input_file).unwrap();
+        let out_name = format!("My{}.xml", origin_name);
+        output_file_path.set_file_name(out_name);
+        println!("output: {}", &output_file_path.display());
+        let tokens = jack_compiler::token::generate_token_list(&mut io.input);
         let xml = to_string(&tokens).unwrap();
-        // let mut out_file = File::create(io.output_file)?;
-        // out_file.write(xml.as_bytes())?;
+        let mut out_file = File::create(output_file_path)?;
+        out_file.write(xml.as_bytes())?;
         println!("{}", xml);
     }
 
--- projects/jack_compiler/tests/main.rs
+++ projects/jack_compiler/tests/main.rs
@@ -1,18 +1,36 @@
+use jack_compiler::{generate_ioset, get_origin_name, token};
+// use quick_xml::de::from_reader;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::PathBuf;
-// mod jack_compiler;
-// use super::*;
+
+const TEST_DIR: &str = "tests";
+const DATA_DIR: &str = "data";
 
 #[test]
 fn test_tokenized_xml() -> Result<(), std::io::Error> {
 	let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 	let dirs = vec!["ArrayTest", "ExpressionLessSquare", "Square"];
 	for dir in dirs {
-		let target = root.join("test/data").join(dir);
+		let target = root.join(TEST_DIR).join(DATA_DIR).join(dir);
+		// println!("{:?}", target);
 		// Convert jack to token xml for each directory
-		let io_list = jack_compiler::generate_ioset(&target)?;
-		println!("{:?}", io_list)
-		// let tokens = generate_token_list(&mut file.input);
-		// let xml = to_string(&tokens).unwrap();
+		let io_list = generate_ioset(&target)?;
+		// println!("{:?}", io_list);
+		for mut io in io_list {
+			let origin = get_origin_name(&io.input_file).unwrap();
+			let mut golden_file_path = io.input_file.clone();
+			let golden_name = format!("{}T.xml", origin);
+			golden_file_path.set_file_name(golden_name);
+			let tokens = token::generate_token_list(&mut io.input);
+
+			// Generate XML from golden results
+			// let golden = File::open(golden_file_path).unwrap();
+			// let reader = BufReader::new(golden);
+			// let list: token::TokenList = from_reader(reader).unwrap();
+
+			// let xml = to_string(&tokens).unwrap();
+		}
 	}
 	Ok(())
 	// Read golden xml
